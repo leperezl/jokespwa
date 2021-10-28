03d5a3a504db12fdfd20c81f9a0a9222491a2a6d
Luis Perez <le.perezl@uniandes.edu.co>
2021-10-28 16:30:46 -0500

    first commit, pwa works except no api yet

---
+++ src/nikaido.js
@@ -0,0 +1,13 @@
+import './styles/jokes.css';
+
+import React from 'react'
+
+const Nikaido = () => {
+    return (
+    <div>
+        <img src="https://i.pinimg.com/originals/c3/1b/d5/c31bd50ac7f78430a994596415740e78.png" alt="nikaido" className="funnyLogo" />
+    </div>
+    )
+}
+
+export default Nikaido
